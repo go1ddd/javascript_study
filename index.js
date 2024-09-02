08316c979c15cffaa552ff77e6fbdfb8a29bcc41
go1ddd <vekasio@naver.com>
2024-09-02 17:06:43 +0900

    자바스크립트 공부 1일차

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('Hello World!');
+console.log(1);
+console.log(true);
+console.log({ name: '홍길동', age: 20, married: false });
+console.log('Hi!', 100, true, [1, 2, 3]);
+
+console.log(this);
